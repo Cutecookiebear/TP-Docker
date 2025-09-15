88effbadaaf1f03b16dfb2fa155ab4c8fc162faa
Cutecookiebear <114440233+Cutecookiebear@users.noreply.github.com>
2025-09-15 10:09:53 +0200

    Ex5 my flask app files
    depot des files ex 5

---
+++ Ex5/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<3 Hello World from Flask in Docker! ^_^"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
